8c87519b8cefedbc3b1a57af7fccaf44e44919e3
Achutha Aswin Naick <aswin.gcsj@gmail.com>
2025-10-27 19:52:10 +0530

    Create circular_time_shift_property.py

---
+++ Experiment 7/circular_time_shift_property.py
@@ -0,0 +1,10 @@
+n0 = 12 #delaying y_in by 12
+y_in_shifted = np.roll(y_in, n0)
+Y_shifted_dft = fft(y_in_shifted)
+
+k = np.arange(N)
+Y_expected_shift_dft = Y * np.exp(-1j * 2 * np.pi * k * n0 / N)
+
+#PTQ_13
+verification_err = np.sum(np.abs(Y_shifted_dft - Y_expected_shift_dft)**2)
+print("Circular time shift verification error:", verification_err)
